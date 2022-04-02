dfdddcf2ab09a98d17de96974247683f83426eb9
eglauko <eglauko@hotmail.com>
2022-04-02 00:46:38 -0300

    app layout sample start to test

--- RoyalCode.Yasamen/RoyalCode.Yasamen.Demos.Wasm/Program.cs
+++ RoyalCode.Yasamen/RoyalCode.Yasamen.Demos.Wasm/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RoyalCode.Yasamen.Commons.Modules;
 using RoyalCode.Yasamen.Demos.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<ScrollJsModule>();
+builder.Services.AddSingleton<ToggleJsModule>();
 
 await builder.Build().RunAsync();
