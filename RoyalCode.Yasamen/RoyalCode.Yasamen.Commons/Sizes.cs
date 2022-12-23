4ec208dab4c2bb261f75ea26e92b0b84edc8b5c5
Glauco Knihs <eglauko@hotmail.com>
2022-12-22 23:12:50 -0300

    Box e não Block. Foram feitas melhorias no Box para atender as necessidades do Block. Mantem-se um componente apenas, Box.

--- RoyalCode.Yasamen/RoyalCode.Yasamen.Commons/Sizes.cs
+++ RoyalCode.Yasamen/RoyalCode.Yasamen.Commons/Sizes.cs
@@ -10,3 +10,27 @@ public enum Sizes
     Large,
     Largest,
 }
+
+public static class SizesExtensions
+{
+    public static string ToAbbrCssClass(this Sizes size)
+    {
+        switch (size)
+        {
+            case Sizes.Default:
+                return string.Empty;
+            case Sizes.Smallest:
+                return "xs";
+            case Sizes.Small:
+                return "sm";
+            case Sizes.Medium:
+                return "md";
+            case Sizes.Large:
+                return "lg";
+            case Sizes.Largest:
+                return "xl";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(size), size, null);
+        }
+    }
+}
