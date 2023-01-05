ec9c7d7f770bbaea2da7edc910e1d21022306cd5
Glauco Knihs <eglauko@hotmail.com>
2023-01-05 00:15:34 -0300

    Buttons created. Next is: validate and PR.

---
+++ RoyalCode.Yasamen/RoyalCode.Yasamen.FormsSandBox/Components/SubmitButton.razor.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using RoyalCode.Yasamen.Layout;
+
+namespace RoyalCode.Yasamen.Forms.Components;
+
+public partial class SubmitButton
+{
+    [MultiplesParameters]
+    public ColumnSizes ColumnSizes { set; get; } = new();
+}
