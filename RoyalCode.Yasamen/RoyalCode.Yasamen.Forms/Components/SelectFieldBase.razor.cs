b21cd179aff948b06fa398900dc31d666e63a3dc
Glauco Knihs <eglauko@hotmail.com>
2023-01-22 11:55:57 -0300

    Select criado, requer estilização

--- RoyalCode.Yasamen/RoyalCode.Yasamen.Forms/Components/SelectFieldBase.razor.cs
+++ RoyalCode.Yasamen/RoyalCode.Yasamen.Forms/Components/SelectFieldBase.razor.cs
@@ -7,46 +7,61 @@ using System.Globalization;
 
 namespace RoyalCode.Yasamen.Forms.Components;
 
-public abstract partial class InputFieldBase<TValue> : FieldBase<TValue>
+
+public sealed class SelectField<TValue> : SelectFieldBase<TValue>
 {
-    protected const string CssScopeAttribute = "b-input-field";
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    protected override int RenderOptions(RenderTreeBuilder builder, int index)
+    {
+        builder.AddContent(index, ChildContent);
+        return index + 1;
+    }
+}
+
+public partial class SelectFieldBase<TValue> : FieldBase<TValue>
+{
+    protected const string CssScopeAttribute = "b-select-field";
 
     protected static readonly Dictionary<string, object> AdditionalContainerAttributes = new()
     {
         {CssScopeAttribute, true}
     };
-
+    
     private readonly RenderFragment contentFragment;
+    private readonly bool isMultipleSelect;
+    private readonly bool isArray;
     private bool isFocused;
-    protected ElementReference InputReference;
+    protected ElementReference SelectReference;
 
-    public InputFieldBase()
+    public SelectFieldBase()
     {
         contentFragment = BuildContent;
+        isArray = typeof(TValue).IsArray;
+        isMultipleSelect = isArray || typeof(TValue) == typeof(IEnumerable<string>);
     }
-
-    private CssClassMap InputCssClasses => CssClassMap.Create("form-control")
-        .Add(() => InputAdditionalClasses)
-        .Add(() => InternalInputClasses)
+    
+    private CssClassMap SelectCssClasses => CssClassMap.Create("form-select")
+        .Add(() => SelectAdditionalClasses)
+        .Add(() => InternalSelectClasses)
         .Add(() => IsInvalid, "is-invalid");
 
     protected virtual bool HasInputGroup => Prepend.IsNotEmptyFragment() || Append.IsNotEmptyFragment();
-
-    public InputType Type { get; protected set; }
-
-    protected string? InternalInputClasses { get; set; }
     
+    protected string? InternalSelectClasses { get; set; }
+
     [MultiplesParameters]
     public ColumnSizes ColumnSizes { set; get; } = new();
 
     [Inject]
     public FormsJsModule Js { get; set; } = null!;
-    
-    [Parameter]
-    public string? LabelAdditionalClasses { get; set; }
 
     [Parameter]
-    public string? InputAdditionalClasses { get; set; }
+    public string? LabelAdditionalClasses { get; set; }
+    
+    [Parameter]
+    public string? SelectAdditionalClasses { get; set; }
 
     [Parameter]
     public RenderFragment? Prepend { get; set; }
@@ -87,7 +102,7 @@ public abstract partial class InputFieldBase<TValue> : FieldBase<TValue>
         }
 
         index = RenderPrepend(builder, index);
-        index = RenderInput(builder, index);
+        index = RenderSelect(builder, index);
         index = RenderAppend(builder, index);
 
         if (hasInputGroup)
@@ -96,7 +111,6 @@ public abstract partial class InputFieldBase<TValue> : FieldBase<TValue>
         }
 
         index = RenderFieldMessages(builder, index);
-        index = RenderLoadingState(builder, index);
         RenderEnd(builder, index);
     }
 
@@ -128,38 +142,48 @@ public abstract partial class InputFieldBase<TValue> : FieldBase<TValue>
         return index + 1;
     }
 
-    protected virtual int RenderInput(RenderTreeBuilder builder, int index)
+    protected virtual int RenderSelect(RenderTreeBuilder builder, int index)
     {
-        builder.OpenElement(index, "input");
+        builder.OpenElement(index, "select");
+        
         builder.AddMultipleAttributes(1 + index, AdditionalAttributes);
         builder.AddAttribute(2 + index, "id", FieldId);
         builder.AddAttribute(3 + index, "name", FieldName);
-        builder.AddAttribute(4 + index, "type", Type.ToString().ToLower());
-        builder.AddAttribute(5 + index, "class", InputCssClasses);
+        builder.AddAttribute(3, "multiple", isMultipleSelect);
+        builder.AddAttribute(5 + index, "class", SelectCssClasses);
         builder.AddAttribute(6 + index, CssScopeAttribute);
 
-        index = RenderInputValueAndBinder(builder, index + 7);
+        if (isMultipleSelect)
+        {
+            builder.AddAttribute(7 + index, "value", BindConverter.FormatValue(CurrentValue)?.ToString());
+            builder.AddAttribute(8 + index, "onchange", EventCallback.Factory.CreateBinder<string?[]?>(this, SetCurrentValueAsStringArray, default));
+        }
+        else
+        {
+            builder.AddAttribute(9 + index, "value", CurrentValueAsString);
+            builder.AddAttribute(10 + index, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, default));
+        }
 
-        builder.AddAttribute(index, "onfocus", EventCallback.Factory.Create(this, OnFocus));
-        builder.AddAttribute(1 + index, "onblur", EventCallback.Factory.Create(this, OnBlur));
-        builder.AddAttribute(2 + index, "onmouseout", EventCallback.Factory.Create(this, OnMouseOut));
-        builder.AddAttribute(3 + index, "onmouseenter", EventCallback.Factory.Create(this, OnMouseEnter));
+        builder.AddAttribute(11 + index, "onfocus", EventCallback.Factory.Create(this, OnFocus));
+        builder.AddAttribute(12 + index, "onblur", EventCallback.Factory.Create(this, OnBlur));
+        builder.AddAttribute(13 + index, "onmouseout", EventCallback.Factory.Create(this, OnMouseOut));
+        builder.AddAttribute(14 + index, "onmouseenter", EventCallback.Factory.Create(this, OnMouseEnter));
         if (ModelContext?.ContainerState.IsLoading ?? false)
-            builder.AddAttribute(4 + index, "disabled", true);
+            builder.AddAttribute(15 + index, "disabled", true);
         if (IsInvalid)
-            builder.AddAttribute(5 + index, "aria-invalid", true);
-        builder.AddElementReferenceCapture(6 + index, __inputReference => InputReference = __inputReference);
+            builder.AddAttribute(16 + index, "aria-invalid", true);
+        builder.AddElementReferenceCapture(17 + index, __ref => SelectReference = __ref);
 
+        RenderOptions(builder, 18 + index);
+        
         builder.CloseElement();
-
-        return index + 7;
+        
+        return index;
     }
 
-    protected virtual int RenderInputValueAndBinder(RenderTreeBuilder builder, int index)
+    protected virtual int RenderOptions(RenderTreeBuilder builder, int index)
     {
-        builder.AddAttribute(index, "value", CurrentValueAsString);
-        builder.AddAttribute(1 + index, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString ?? string.Empty));
-        return index + 2;
+        return index;
     }
 
     protected virtual int RenderAppend(RenderTreeBuilder builder, int index)
@@ -177,24 +201,8 @@ public abstract partial class InputFieldBase<TValue> : FieldBase<TValue>
         builder.AddAttribute(1 + index, "FieldIdentifier", FieldIdentifier);
         builder.AddAttribute(2 + index, "FieldName", FieldName);
         builder.CloseComponent();
-        
+
         return index + 3;
-    }
-
-    protected virtual int RenderLoadingState(RenderTreeBuilder builder, int index)
-    {
-        // TODO: requer adição correta das classes, segundo alinhamento, esquerda, direita...
-
-        if (ModelContext?.ContainerState.IsLoading ?? false)
-        {
-            builder.OpenElement(index, "div");
-            builder.AddAttribute(1 + index, "class", "spinner-border spinner-border-sm");
-            builder.AddAttribute(2 + index, "role", "status");
-            builder.AddAttribute(3 + index, "aria-hidden", "true");
-            builder.AddAttribute(4 + index, CssScopeAttribute);
-            builder.CloseElement();
-        }
-        return index + 5;
     }
 
     protected virtual void RenderEnd(RenderTreeBuilder builder, int index) { }
@@ -203,10 +211,17 @@ public abstract partial class InputFieldBase<TValue> : FieldBase<TValue>
     {
         if (firstRender)
         {
-            await Js.BlurOnPressEnterAsync(InputReference);
+            await Js.BlurOnPressEnterAsync(SelectReference);
         }
     }
-    
+
+    private void SetCurrentValueAsStringArray(string?[]? value)
+    {
+        CurrentValue = BindConverter.TryConvertTo<TValue>(value, CultureInfo.CurrentCulture, out var result)
+            ? result
+            : default;
+    }
+
     private void OnFocus()
     {
         isFocused = true;
