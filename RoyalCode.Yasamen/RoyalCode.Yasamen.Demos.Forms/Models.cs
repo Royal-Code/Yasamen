81faf311d7e7ebd720d2687c957e3430f3fbf031
Glauco Knihs <eglauko@hotmail.com>
2023-01-25 20:39:58 -0300

    Teste concluído

--- RoyalCode.Yasamen/RoyalCode.Yasamen.Demos.Forms/Models.cs
+++ RoyalCode.Yasamen/RoyalCode.Yasamen.Demos.Forms/Models.cs
@@ -26,6 +26,8 @@ public class Friend
     public int CityId { get; set; }
 
     public int? AlterCityId { get; set; }
+
+    public Car Car { get; set; }
 }
 
 
@@ -55,6 +57,13 @@ public class City
     public string Name { get; set; } = null!;
 }
 
+public class Car
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
+
 public class ValidadorFriend : IValidator<Friend>
 {
     public bool Failure { get; set; }
@@ -74,7 +83,7 @@ public class ValidadorFriend : IValidator<Friend>
     }
 }
 
-public class CityServices
+public class CityService
 {
     private static readonly List<City> cities = new List<City>
     {
@@ -135,5 +144,69 @@ public class CityServices
     {
         await Task.Delay(3_000);
         return cities;
+    }
+}
+
+public class CarService
+{
+    private static readonly List<Car> cars = new List<Car>
+    {
+        new Car()
+        {
+            Id = 1,
+            Name = "Ferrari"
+        },
+        new Car()
+        {
+            Id = 2,
+            Name = "Lamborghini"
+        },
+        new Car()
+        {
+            Id = 3,
+            Name = "Porsche"
+        },
+        new Car()
+        {
+            Id = 4,
+            Name = "Audi"
+        },
+        new Car()
+        {
+            Id = 5,
+            Name = "BMW"
+        },
+        new Car()
+        {
+            Id = 6,
+            Name = "Mercedes-Benz"
+        },
+        new Car()
+        {
+            Id = 7,
+            Name = "Volkswagen"
+        },
+        new Car()
+        {
+            Id = 8,
+            Name = "Toyota"
+        },
+        new Car()
+        {
+            Id = 9,
+            Name = "Honda"
+        },
+        new Car()
+        {
+            Id = 10,
+            Name = "Hyundai"
+        }
+    };
+
+    [Loader]
+    public async Task<IEnumerable<Car>> GetCars()
+    {
+        await Task.Delay(3_000);
+        return cars;
     }
 }
