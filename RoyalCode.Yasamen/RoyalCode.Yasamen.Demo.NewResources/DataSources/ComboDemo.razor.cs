0c3ecf21c87929989319198f5154f518481536d4
eglauko <eglauko@hotmail.com>
2024-11-22 18:05:18 -0300

    ok

---
+++ RoyalCode.Yasamen/RoyalCode.Yasamen.Demo.NewResources/DataSources/ComboDemo.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace RoyalCode.Yasamen.Demo.NewResources.DataSources;
+
+public partial class ComboDemo<T> where T : class
+{
+    [Inject]
+    public IResources Resources { get; set; }
+
+    [Parameter]
+    public T Selected { get; set; }
+
+    /// <summary>
+    /// Gets or sets a callback that updates the bound value.
+    /// </summary>
+    [Parameter]
+    public EventCallback<T> SelectedChanged { get; set; }
+
+    /// <summary>
+    /// Gets or sets an expression that identifies the bound value.
+    /// </summary>
+    [Parameter]
+    public Expression<Func<T>> SelectedExpression { get; set; }
+
+    [Parameter]
+    public Func<T, object> Key { get; set; }
+
+    [Parameter]
+    public Func<T, string> Description { get; set; }
+
+    [Parameter]
+    public IEnumerable<T> Items { get; set; }
+
+    [Parameter]
+    public IResourceList<T> Resource { get; set; }
+
+    [CascadingParameter]
+    public IResourceList<T> CascadingResource { get; set; }
+}
